4b73db8ad23af6cf58d124a0fcfdf37660f4042f
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-07-01 09:19:51 +0300

    Add function for json read

--- bot/bot_helpers.py
+++ bot/bot_helpers.py
@@ -1,7 +1,11 @@
+import json
 from functools import wraps
+from pathlib import Path
 
 # TODO: читать администраторов из json
 LIST_OF_ADMINS = [12345678, 87654321, 511727477]
+DATA_FOLDER = 'data'
+
 
 def restricted(func):
     """Запрет доступа к обработчику для не-администраторов"""
@@ -22,4 +26,11 @@ def is_user_admin(user_id: int):
     if user_id in LIST_OF_ADMINS:
         return True
     return False
+
+
     
+def read_json(filename: str):
+    """Десереализовать JSON"""
+    with open(Path.cwd() / Path(DATA_FOLDER) / filename, "r", encoding='utf8') as file_:
+        file_json = file_.read()
+    return json.loads(file_json)
