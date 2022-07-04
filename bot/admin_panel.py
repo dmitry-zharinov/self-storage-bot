4de8452441c85eed4bc0466cce0fe95d77f08c81
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-07-04 12:35:46 +0300

    Show overdue orders

--- bot/admin_panel.py
+++ bot/admin_panel.py
@@ -1,13 +1,17 @@
+import datetime
 from functools import wraps
 
 from environs import Env
-from telegram import ReplyKeyboardMarkup, Update
+from telegram import ParseMode, ReplyKeyboardMarkup, Update
 from telegram.ext import CallbackContext
+
+from .bot_helpers import read_json
 
 env = Env()
 env.read_env()
 
 LIST_OF_ADMINS = list(map(int, env.list('LIST_OF_ADMINS')))
+ORDERS_FILENAME = 'orders.json'
 
 
 def get_main_menu(user_id: int) -> ReplyKeyboardMarkup:
@@ -56,9 +60,31 @@ def open_admin_panel(update: Update, context: CallbackContext):
 
 @restricted
 def show_overdue_orders(update: Update, context: CallbackContext):
+    """Показать просроченные заказы"""
+    overdue_orders = []
+    orders: dict = read_json(ORDERS_FILENAME)
+    current_date = datetime.datetime.today().date()
+    for order, info in orders.items():
+        end_time = info.get('end_time')
+
+        if end_time:
+            if datetime.datetime.fromisoformat(end_time).date() < current_date:
+                user_id = info.get('user_id')
+                overdue_orders.append(
+                    f'<b>Заказ {order}</b>\n'
+                    f'Клиент {info.get("user_name")}\n'
+                    f'Telegram ID: <a href="tg://user?id={user_id}">{user_id}</a>\n'
+                    f'Номер телефона: {info.get("feedback")}\n'
+                    f'Заказ истёк: {info.get("end_time")}')
+    if not overdue_orders:
+        overdue_orders_msg = 'Нет просроченных заказов'
+    else:
+        overdue_orders_msg = '\n'.join(overdue_orders)
+
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='Тут будет список просроченных заказов',
+        text=overdue_orders_msg,
+        parse_mode=ParseMode.HTML,
         reply_markup=get_admin_keyboard())
 
 
